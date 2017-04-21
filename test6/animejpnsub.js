0e35541c9c972678a7216385ea1223af75bd47f6
arcane12 <rcaintwelve@gmail.com>
2017-04-21 09:43:05 +0900

    Update animejpnsub.js

--- test6/animejpnsub.js
+++ test6/animejpnsub.js
@@ -1,133 +1,174 @@
-<script type="text/javascript">
+"use strict"
 
-/*-----   Change info here   -----*/
-var anime_title = "template"
-var jlinks = {
-  "episode01": {
-    "name": "01",
-    "video": "https://lh3.googleusercontent.com/xH65expd0WkI7FjDCnQ9YmRuPuRI8zwnVdIdDpD6QZjjcoUNhHrCiQ-wEaF1mMHienk6PVb97_zjv6uAKn0flonBZusPCvU3mR6FqsX8fTacFPe7HYFvQZiBQx-P4BTma4EUDQ=m22",
-    "subtitles": {
-      "Japanese": "https://dl.dropboxusercontent.com/s/dckm0stg56zbbai/witch1.vtt",
-      "English": "https://dl.dropboxusercontent.com/s/0x4u50qgd7n6t2b/witch1%20eng.vtt"
-    }
-  },
-  "episode02": {
-    "name": "02",
-    "video": "https://lh3.googleusercontent.com/76eVBU66ka8ryaLvFeC0KckdPNArxFQXeYcl04z5X3y6wTJ90ZRL96NI99XpNClL7XKW1mH086XRXia9HluNy4zuFHXpPk5KymGlbzw4RRwQcKxUQmgB3aWvHiPyk-l_pH990g=m22",
-    "subtitles": {
-      "Japanese": "https://dl.dropboxusercontent.com/s/5efnetbp1bimq68/witch2.vtt",
-      "English": "https://dl.dropboxusercontent.com/s/72hu5pwnwoi4d0b/witch2%20eng.vtt"
-    }
-  },
-  "episode03": {
-    "name": "03",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode04": {
-    "name": "04",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode05": {
-    "name": "05",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode06": {
-    "name": "06",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode07": {
-    "name": "07",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode08": {
-    "name": "08",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode09": {
-    "name": "09",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode10": {
-    "name": "10",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode11": {
-    "name": "11",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  },
-  "episode12": {
-    "name": "12",
-    "video": "",
-    "subtitles": {
-      "Japanese": "",
-      "English": ""
-    }
-  }
-};
-</script>
+if ( getQueryFieldValue("v") && jlinks[getQueryFieldValue("v")] !== undefined) {
+	var episode = getQueryFieldValue("v");
 
-<!-- Title and Links -->
-<h2>NOW WATCHING:&nbsp;<span style="color: #990000; font-size: x-large;" id="video-title"></span></h2>
-<div class="content" style="background-color: white; font-family: arial, tahoma, geneva, sans-serif; padding: 10px; font-weight: bold;">
-	<span style="font-size: large;" id="episode-links">Eps:&nbsp;</span>
-</div>
+	// Add titles
+	document.title = anime_title + " " + jlinks[episode].name + " - Anime Japanese Subtitles";
+	$("#video-title").html(anime_title + " " + jlinks[episode].name);
+	
+	// Add links
+	Object.keys(jlinks).forEach(function(key){
+		if (key == episode)
+			$("#episode-links").append("&nbsp;", jlinks[episode].name, "&nbsp;");
+		else
+			$("#episode-links").append("&nbsp;", $("<a></a>", { href: "?v=" + key }).html(jlinks[key].name), "&nbsp;");
+	});
+	
+	// Initialize VideoJS
+	var vjs = videojs(document.getElementById("videoPlayer"), {
+		"html5": { nativeTextTracks: false }
+	});
+	
+	// Add source
+	vjs.src({type: "video/mp4", src: jlinks[episode].video});
+	
+	// Load subtitles
+	loadSubs();
+	
+	vjs.ready(function() {
+		// Enable hotkeys
+		this.hotkeys({
+			volumeStep: 0.1,
+			seekStep: 5,
+			alwaysCaptureHotkeys: true,
+			enableVolumeScroll: false,
+			enableNumbers: false,
+			enableModifiersForNumbers: false,
+			customKeys: {
+				ccKey: {
+					key: function(event) {
+						// Toggle something with C Key
+						return (event.which === 67);
+					},
+					handler: function(player, options, event) {
+						// Toggles the CC button
+						var tracks = vjs.textTracks();
+						for (var i=0; i < tracks.length; i++) {
+							var track = tracks[i];
+							if (track.mode === "showing") {
+								// disable current
+								track.mode = "disabled";
+								if ( i < tracks.length - 1) {
+									// enable next if current is not last
+									tracks[i+1].mode = "showing";
+								}
+								break
+								return
+							} else if ( i === tracks.length - 1 ) {
+								// enable first if all disabled
+								tracks[0].mode = "showing";
+								break
+								return
+							}
+						}
+					}
+				}
+			}
+		});
+		
+		// Show video
+		$("#loading-message").remove();
+		$("#video-container").show();
+	});
+	
+} else {
+	window.location.href = "?v=" + Object.keys(jlinks)[0];
+}
 
-<!-- Video and Subtitles -->
-<div id="div-left" style="float:left">
-	<div id="loading-message" style="margin: 0 auto; width:640px; text-align:center;">Loading...</div>
-	<div id="video-container" style="display:none;">
-		<video id="videoPlayer" class="video-js vjs-big-play-centered" width="640" height="360" controls></video>
-	</div>
-	<div id="subtitles_display" class="advanced_subtitles" style="text-align:center;"></div>
-</div>
-<div id="div-right" style="float:left; text-align:center; width:300px">
-	<input id="subtitles_button" value="Click to Hide/Show Advanced Subtitles" type="button" onclick="toggleAdvSub()">
-	<div id="subtitles-navigation" class="advanced_subtitles">
-		<div>
-			<select size='5' id='results' onchange='playCaption(this)' style='width:100%; height:240px' ></select>
-		</div>
-		<div style="color:#0000FF; font-size:0.75em; margin-top:6px;">Click a line to jump to that point in the video.<br>Then use up/down arrows to navigate by line.</div>
-		<hr>
-		<div id="subtitles_controls"></div>
-	</div>
-</div>
+function loadSubs() {
+	var l=-1;
+	Object.keys(jlinks[episode].subtitles).forEach(function(label){
+		l++;
+		
+		// Add tracks
+		vjs.addRemoteTextTrack({
+			src: jlinks[episode].subtitles[label],
+			label: label,
+			kind: "captions"
+		});
+		
+		var track = vjs.textTracks()[l];
+		
+		// Initialize track
+		track.mode = "showing";
+		track.mode = "disabled";
+			
+		// Add display
+		$("<div></div>", {
+			id: "pre_"+label,
+			style: "white-space:pre;" })
+			.html("<br>")
+			.appendTo( $("#subtitles_display") )
+			.hide();
+		
+		// Add checkboxes
+		$("<label></label>", {
+			for: "box"+label,
+			style: "display:inline-block" })
+			.append( $("<input>", {
+				type: "checkbox",
+				id: "box"+label,
+				style: "margin-left: 25px",
+				value: label })
+				.change(function() {
+					if (this.checked)
+						$("#pre_"+this.value).show();
+					else
+						$("#pre_"+this.value).hide();
+				})
+			,label)
+			.appendTo( $("#subtitles_controls") );
+		
+		if (label == "Japanese") {
+			track.on("loadeddata", function(){
+				// Automatically display Japanese
+				$("#box"+this.label).attr("checked", true);
+				$("#pre_"+this.label).show();
+				
+				// Load Japanese captions
+				loadCaptions(this.cues);
+			});
+		}
+		
+		// Update display
+		track.addEventListener('cuechange', function() {
+			var cue = this.activeCues[0];
+			var pre = document.getElementById("pre_"+this.label);
+			if (cue) {
+				pre.innerHTML = "";
+				pre.appendChild(cue.getCueAsHTML());
+			} else
+				pre.innerHTML = "<br>";
+		});
+		
+	});
+}
 
-<script src="https://vjs.zencdn.net/5.8.8/video.js"></script>
-<script src="https://cdn.sc.gl/videojs-hotkeys/latest/videojs.hotkeys.min.js"></script>
-<script src="https://gitcdn.link/repo/arcane12/animejpnsub/master/test6/animejpnsub.js"></script>
+function loadCaptions(cues) {
+	for (var i=0; i < cues.length; i++) {
+		var x = cues[i].getCueAsHTML();
+		var option = document.createElement("option");
+		option.text = x.textContent;
+		option.setAttribute('data-time', cues[i].startTime);
+		document.getElementById('results').add(option);
+	}
+}
+
+function playCaption(control) {
+	var t = control.options[control.options.selectedIndex].getAttribute('data-time');
+	vjs.currentTime(t);
+}
+
+function toggleAdvSub() {
+	$(".advanced_subtitles").toggle();
+}
+
+function getQueryFieldValue(field) {
+	var query = window.location.search.substring(1).split("&");
+	for (var i=0; i < query.length; i++) {
+		var pair = query[i].split("=");
+		if(pair[0] == field){return pair[1];}
+	}
+	return(false);
+}
